e98070c9dfd5cff5432b9ec3f93d351c629b51d1
IkeVoodoo2 <simone.coletti07@gmail.com>
2022-02-23 16:05:25 +0100

    Update CommandRegistry.java

--- src/main/java/me/ikevoodoo/plugincore/CommandRegistry.java
+++ src/main/java/me/ikevoodoo/plugincore/CommandRegistry.java
@@ -64,88 +64,13 @@ public class CommandRegistry {
     private record CommandData(Method method, ParameterType[] types, Object obj) {
 
         public boolean execute(CommandSender sender, String label, String[] args) {
-            try {
-                Object[] params = new Object[types.length];
-                for (int i = 0; i < types.length; i++) {
-                    switch (types[i]) {
-                        case SENDER -> params[i] = sender;
-                        case LABEL -> params[i] = label;
-                        case STRING_ARGS -> params[i] = args;
-                        case ARGS -> params[i] = new Arguments(args);
-                    }
-                }
-                Object result = method.invoke(obj, params);
-
-                if (result == null) {
-                    return true;
-                }
-
-                if (result instanceof String text) {
-                    sender.sendMessage(ChatUtils.color(text));
-                }
-
-                if (result instanceof List list) {
-                    if(list.size() > 0) {
-                        Object first = list.get(0);
-                        if(first instanceof String) {
-                            for (Object o : list) {
-                                sender.sendMessage(ChatUtils.color((String) o));
-                            }
-                        }
-                    }
-                }
-
-                if (result instanceof Boolean bool) {
-                    return bool;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return true;
+            return getInstance().execute(method, types, obj, sender, label, args);
         }
     }
 
     private record SubCommandData(SubCommand command, Method method, ParameterType[] types, Object obj) {
         public boolean execute(CommandSender sender, String label, String[] args) {
-            method.setAccessible(true);
-            try {
-                Object[] params = new Object[types.length];
-                for (int i = 0; i < types.length; i++) {
-                    switch (types[i]) {
-                        case SENDER -> params[i] = sender;
-                        case LABEL -> params[i] = label;
-                        case STRING_ARGS -> params[i] = args;
-                        case ARGS -> params[i] = new Arguments(args);
-                    }
-                }
-                Object result = method.invoke(obj, params);
-
-                if (result == null) {
-                    return true;
-                }
-
-                if (result instanceof String text) {
-                    sender.sendMessage(ChatUtils.color(text));
-                }
-
-                if (result instanceof List list) {
-                    if(list.size() > 0) {
-                        Object first = list.get(0);
-                        if(first instanceof String) {
-                            for (Object o : list) {
-                                sender.sendMessage(ChatUtils.color((String) o));
-                            }
-                        }
-                    }
-                }
-
-                if (result instanceof Boolean bool) {
-                    return bool;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return true;
+            return getInstance().execute(method, types, obj, sender, label, args);
         }
     }
 
@@ -178,6 +103,10 @@ public class CommandRegistry {
             String[] cmdArgs = remaining == null ? new String[0] : remaining.toArray(new String[0]);
 
             SubCommandData subCommand = paths.get(path);
+            if(!getInstance().canUse(sender, subCommand.command().usableBy(), subCommand.command().permission())) {
+                return true;
+            }
+
             if(!subCommand.command().redirectPath().isBlank()) {
                 subCommand.execute(sender, label, cmdArgs);
                 String redirect = subCommand.command().redirectPath();
@@ -186,7 +115,6 @@ public class CommandRegistry {
                 }
                 subCommand = paths.get(redirect);
             }
-
 
             return subCommand.execute(sender, label, cmdArgs);
         }
@@ -210,36 +138,7 @@ public class CommandRegistry {
         for (Method method : clazz.getDeclaredMethods()) {
             if (method.isAnnotationPresent(SubCommand.class)) {
                 SubCommand subCommand = method.getAnnotation(SubCommand.class);
-                int[] paramIndexes = new int[4];
-                Arrays.fill(paramIndexes, -1);
-
-                List<ParameterType> types = new ArrayList<>();
-
-                int i = 0;
-                for(Class<?> param : method.getParameterTypes()) {
-                    if(param == CommandSender.class && paramIndexes[0] == -1) {
-                        paramIndexes[0] = i;
-                        types.add(ParameterType.SENDER);
-                    }
-
-                    if(param == String.class && paramIndexes[1] == -1) {
-                        paramIndexes[1] = i;
-                        types.add(ParameterType.LABEL);
-                    }
-
-                    if(param == String[].class && paramIndexes[2] == -1) {
-                        paramIndexes[2] = i;
-                        types.add(ParameterType.STRING_ARGS);
-                    }
-
-                    if(param == Arguments.class && paramIndexes[3] == -1) {
-                        paramIndexes[3] = i;
-                        types.add(ParameterType.ARGS);
-                    }
-                    i++;
-                }
-
-                SubCommandData data = new SubCommandData(subCommand, method, types.toArray(new ParameterType[0]), obj);
+                SubCommandData data = new SubCommandData(subCommand, method, getTypes(method).toArray(new ParameterType[0]), obj);
                 subCommands.add(data);
             }
         }
@@ -252,36 +151,7 @@ public class CommandRegistry {
             if(m.isAnnotationPresent(Command.class)) {
                 Command annotation = m.getAnnotation(Command.class);
 
-                int[] paramIndexes = new int[4];
-                Arrays.fill(paramIndexes, -1);
-
-                List<ParameterType> types = new ArrayList<>();
-
-                int i = 0;
-                for(Class<?> param : m.getParameterTypes()) {
-                    if(param == CommandSender.class && paramIndexes[0] == -1) {
-                        paramIndexes[0] = i;
-                        types.add(ParameterType.SENDER);
-                    }
-
-                    if(param == String.class && paramIndexes[1] == -1) {
-                        paramIndexes[1] = i;
-                        types.add(ParameterType.LABEL);
-                    }
-
-                    if(param == String[].class && paramIndexes[2] == -1) {
-                        paramIndexes[2] = i;
-                        types.add(ParameterType.STRING_ARGS);
-                    }
-
-                    if(param == Arguments.class && paramIndexes[3] == -1) {
-                        paramIndexes[3] = i;
-                        types.add(ParameterType.ARGS);
-                    }
-                    i++;
-                }
-
-                CommandData data = new CommandData(m, types.toArray(new ParameterType[0]), obj);
+                CommandData data = new CommandData(m, getTypes(m).toArray(new ParameterType[0]), obj);
                 registerCommand(annotation, data);
             }
         }
@@ -296,27 +166,8 @@ public class CommandRegistry {
         BukkitCommand command = new BukkitCommand(cmd.name()) {
             @Override
             public boolean execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
-                if(cmd.usableBy() != CommandUser.ALL)  {
-                    if(cmd.usableBy() == CommandUser.CONSOLE) {
-                        if(!(sender instanceof ConsoleCommandSender)) {
-                            sender.sendMessage(ChatUtils.color("&cYou must be the console to use this command!"));
-                            return true;
-                        }
-                    }
-
-                    if(cmd.usableBy() == CommandUser.PLAYER) {
-                        if(!(sender instanceof Player)) {
-                            sender.sendMessage(ChatUtils.color("&cYou must be a player to use this command!"));
-                            return true;
-                        }
-                    }
-
-                    if(cmd.usableBy() == CommandUser.COMMAND_BLOCK) {
-                        if(!(sender instanceof BlockCommandSender)) {
-                            sender.sendMessage(ChatUtils.color("&cYou must be a command block to use this command!"));
-                            return true;
-                        }
-                    }
+                if(!canUse(sender, cmd.usableBy(), null)) {
+                    return true;
                 }
                 return data.execute(sender, label, args);
             }
@@ -333,27 +184,8 @@ public class CommandRegistry {
         BukkitCommand command = new BukkitCommand(cmd.name()) {
             @Override
             public boolean execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
-                if(cmd.usableBy() != CommandUser.ALL)  {
-                    if(cmd.usableBy() == CommandUser.CONSOLE) {
-                        if(!(sender instanceof ConsoleCommandSender)) {
-                            sender.sendMessage(ChatUtils.color("&cYou must be the console to use this command!"));
-                            return true;
-                        }
-                    }
-
-                    if(cmd.usableBy() == CommandUser.PLAYER) {
-                        if(!(sender instanceof Player)) {
-                            sender.sendMessage(ChatUtils.color("&cYou must be a player to use this command!"));
-                            return true;
-                        }
-                    }
-
-                    if(cmd.usableBy() == CommandUser.COMMAND_BLOCK) {
-                        if(!(sender instanceof BlockCommandSender)) {
-                            sender.sendMessage(ChatUtils.color("&cYou must be a command block to use this command!"));
-                            return true;
-                        }
-                    }
+                if(!canUse(sender, cmd.usableBy(), cmd.permission())) {
+                    return true;
                 }
                 return data.execute(sender, label, args);
             }
@@ -366,8 +198,109 @@ public class CommandRegistry {
         commandMap.register(cmd.prefix(), command);
     }
 
-    private boolean canUse(CommandSender sender, CommandUser user) {
-        
+    private boolean execute(Method method, ParameterType[] types, Object obj, CommandSender sender, String label, String[] args) {
+        method.setAccessible(true);
+        try {
+            Object[] params = new Object[types.length];
+            for (int i = 0; i < types.length; i++) {
+                switch (types[i]) {
+                    case SENDER -> params[i] = sender;
+                    case LABEL -> params[i] = label;
+                    case STRING_ARGS -> params[i] = args;
+                    case ARGS -> params[i] = new Arguments(args);
+                }
+            }
+            Object result = method.invoke(obj, params);
+
+            if (result == null) {
+                return true;
+            }
+
+            if (result instanceof String text) {
+                sender.sendMessage(ChatUtils.color(text));
+            }
+
+            if (result instanceof List list) {
+                if(list.size() > 0) {
+                    Object first = list.get(0);
+                    if(first instanceof String) {
+                        for (Object o : list) {
+                            sender.sendMessage(ChatUtils.color((String) o));
+                        }
+                    }
+                }
+            }
+
+            if (result instanceof Boolean bool) {
+                return bool;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    private List<ParameterType> getTypes(Method m) {
+        int[] paramIndexes = new int[4];
+        Arrays.fill(paramIndexes, -1);
+
+        List<ParameterType> types = new ArrayList<>();
+
+        int i = 0;
+        for(Class<?> param : m.getParameterTypes()) {
+            if(param == CommandSender.class && paramIndexes[0] == -1) {
+                paramIndexes[0] = i;
+                types.add(ParameterType.SENDER);
+            }
+
+            if(param == String.class && paramIndexes[1] == -1) {
+                paramIndexes[1] = i;
+                types.add(ParameterType.LABEL);
+            }
+
+            if(param == String[].class && paramIndexes[2] == -1) {
+                paramIndexes[2] = i;
+                types.add(ParameterType.STRING_ARGS);
+            }
+
+            if(param == Arguments.class && paramIndexes[3] == -1) {
+                paramIndexes[3] = i;
+                types.add(ParameterType.ARGS);
+            }
+            i++;
+        }
+        return types;
+    }
+
+    private boolean canUse(CommandSender sender, CommandUser user, String permission) {
+        if(permission != null && !sender.hasPermission(permission)) {
+            sender.sendMessage(ChatUtils.color("&cYou do not have permission to use this command!"));
+            return false;
+        }
+
+        if(user != CommandUser.ALL)  {
+            if(user == CommandUser.CONSOLE) {
+                if(!(sender instanceof ConsoleCommandSender)) {
+                    sender.sendMessage(ChatUtils.color("&cYou must be the console to use this command!"));
+                    return false;
+                }
+            }
+
+            if(user == CommandUser.PLAYER) {
+                if(!(sender instanceof Player)) {
+                    sender.sendMessage(ChatUtils.color("&cYou must be a player to use this command!"));
+                    return false;
+                }
+            }
+
+            if(user == CommandUser.COMMAND_BLOCK) {
+                if(!(sender instanceof BlockCommandSender)) {
+                    sender.sendMessage(ChatUtils.color("&cYou must be a command block to use this command!"));
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
