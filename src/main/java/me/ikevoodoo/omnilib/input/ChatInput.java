797f77a3ae46d00ef2e4f8474be7d099b4c16ac9
Enter-a-username-lol <104366162+Enter-a-username-lol@users.noreply.github.com>
2022-04-25 15:15:02 +0200

    Update ChatInput.java

--- src/main/java/me/ikevoodoo/omnilib/input/ChatInput.java
+++ src/main/java/me/ikevoodoo/omnilib/input/ChatInput.java
@@ -1,10 +1,16 @@
 package me.ikevoodoo.omnilib.input;
 
 import org.bukkit.Player;
-
+import org.bukkit.ChatColor;
 
 public class ChatInput {
     private ChatInput() {}
 
-    private static void askQuestion(String question, Player player) {}
+    private List<Player> players;
+
+    private static void askQuestion(String question, Player player) {
+        player.sendMessage(question);
+        player.sendMessage(ChatColor.GREEN + "Type a message in chat to reply!")
+        
+    }
 }
