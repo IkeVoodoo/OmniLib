8a17c15e4ede7c3536fbad528edca7668d556c61
Enter-a-username-lol <104366162+Enter-a-username-lol@users.noreply.github.com>
2022-04-25 15:02:43 +0200

    Create ChatInput.java

---
+++ src/main/java/me/ikevoodoo/omnilib/input/ChatInput.java
@@ -0,0 +1,10 @@
+package me.ikevoodoo.omnilib.input;
+
+import org.bukkit.Player;
+
+
+public class ChatInput {
+    private ChatInput() {}
+
+    private static void askQuestion(String question, Player player) {}
+}
